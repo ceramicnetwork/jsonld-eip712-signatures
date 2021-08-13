ad06c80e28626452e4caead6e1d7e1a01d9f2f77
Haardik <hhaardik@uwaterloo.ca>
2021-08-13 14:14:22 +0530

    add more test cases

--- src/__tests__/EthereumEip712Signature2021.test.ts
+++ src/__tests__/EthereumEip712Signature2021.test.ts
@@ -93,7 +93,7 @@ describe("EthereumEip712Signature2021", () => {
 
     expect(valid).toEqual(true);
   });
-  it("should create proof", async () => {
+  it("should create and verify proof", async () => {
     const wallet = Wallet.createRandom();
     const address = wallet.address;
     const vm = `did:ethr:${address}#controller`;
@@ -135,10 +135,193 @@ describe("EthereumEip712Signature2021", () => {
       console.error(error);
     }
   });
-  it("should successfully be verified", () => {});
-  it("should fail to verify invalid sig", () => {});
-  it("should fail to verify with invalid doc", () => {});
-  it("should fail to verify with modified doc", () => {});
-  it("should sign nested verifiable credential", () => {});
-  it("should verify nested verifiable credential", () => {});
+  it("should create and verify proof over nested data", async () => {
+    const wallet = Wallet.createRandom();
+    const address = wallet.address;
+    const vm = `did:ethr:${address}#controller`;
+    const date = new Date();
+
+    const s = new EthereumEip712Signature2021({
+      signer: wallet,
+      verificationMethod: vm,
+      date,
+    });
+
+    const jsonLdDocument = {
+      "@context": ["https://schema.org", "https://w3id.org/security/v2"],
+      "@type": "Person",
+      data: {
+        name: {
+          firstName: "John",
+          lastName: "Doe",
+        },
+        job: {
+          jobTitle: "Professor",
+          employer: "University of Waterloo",
+        },
+      },
+      telephone: "(425) 123-4567",
+    };
+
+    try {
+      const res = await s.createProof({
+        document: jsonLdDocument,
+        purpose: new purposes.AssertionProofPurpose(),
+        compactProof: false,
+        documentLoader: customDocLoader,
+      });
+
+      const v = await s.verifyProof({
+        proof: res,
+        document: jsonLdDocument,
+        purpose: new purposes.AssertionProofPurpose(),
+        documentLoader: customDocLoader,
+      });
+
+      expect(v.verified).toEqual(true);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+  it("should fail to verify invalid sig", async () => {
+    const wallet = Wallet.createRandom();
+    const address = wallet.address;
+    const vm = `did:ethr:${address}#controller`;
+    const date = new Date();
+
+    const s = new EthereumEip712Signature2021({
+      signer: wallet,
+      verificationMethod: vm,
+      date,
+    });
+
+    const jsonLdDocument = {
+      "@context": ["https://schema.org", "https://w3id.org/security/v2"],
+      "@type": "Person",
+      firstName: "Jane",
+      lastName: "Does",
+      jobTitle: "Professor",
+      telephone: "(425) 123-4567",
+      email: "jane.doe@example.com",
+    };
+
+    try {
+      const proof = await s.createProof({
+        document: jsonLdDocument,
+        purpose: new purposes.AssertionProofPurpose(),
+        compactProof: false,
+        documentLoader: customDocLoader,
+      });
+
+      proof.proofValue = "abc";
+
+      const v = await s.verifyProof({
+        proof: proof,
+        document: jsonLdDocument,
+        purpose: new purposes.AssertionProofPurpose(),
+        documentLoader: customDocLoader,
+      });
+
+      expect(v.verified).toEqual(false);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+  it("should fail to verify with invalid doc", async () => {
+    const wallet = Wallet.createRandom();
+    const address = wallet.address;
+    const vm = `did:ethr:${address}#controller`;
+    const date = new Date();
+
+    const s = new EthereumEip712Signature2021({
+      signer: wallet,
+      verificationMethod: vm,
+      date,
+    });
+
+    const jsonLdDocument = {
+      "@context": ["https://schema.org", "https://w3id.org/security/v2"],
+      "@type": "Person",
+      firstName: "Jane",
+      lastName: "Does",
+      jobTitle: "Professor",
+      telephone: "(425) 123-4567",
+      email: "jane.doe@example.com",
+    };
+
+    const invalidDocument = {
+      "@context": ["https://schema.org", "https://w3id.org/security/v2"],
+      "@type": "Person",
+      firstName: "Jane",
+    };
+
+    try {
+      const proof = await s.createProof({
+        document: jsonLdDocument,
+        purpose: new purposes.AssertionProofPurpose(),
+        compactProof: false,
+        documentLoader: customDocLoader,
+      });
+
+      proof.proofValue = "abc";
+
+      const v = await s.verifyProof({
+        proof: proof,
+        document: invalidDocument,
+        purpose: new purposes.AssertionProofPurpose(),
+        documentLoader: customDocLoader,
+      });
+
+      expect(v.verified).toEqual(false);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+  it("should fail to verify with modified doc", async () => {
+    const wallet = Wallet.createRandom();
+    const address = wallet.address;
+    const vm = `did:ethr:${address}#controller`;
+    const date = new Date();
+
+    const s = new EthereumEip712Signature2021({
+      signer: wallet,
+      verificationMethod: vm,
+      date,
+    });
+
+    const jsonLdDocument = {
+      "@context": ["https://schema.org", "https://w3id.org/security/v2"],
+      "@type": "Person",
+      firstName: "Jane",
+      lastName: "Does",
+      jobTitle: "Professor",
+      telephone: "(425) 123-4567",
+      email: "jane.doe@example.com",
+    };
+
+    try {
+      const proof = await s.createProof({
+        document: jsonLdDocument,
+        purpose: new purposes.AssertionProofPurpose(),
+        compactProof: false,
+        documentLoader: customDocLoader,
+      });
+
+      proof.proofValue = "abc";
+
+      const v = await s.verifyProof({
+        proof: proof,
+        document: {
+          ...jsonLdDocument,
+          firstName: "John",
+        },
+        purpose: new purposes.AssertionProofPurpose(),
+        documentLoader: customDocLoader,
+      });
+
+      expect(v.verified).toEqual(false);
+    } catch (error) {
+      console.error(error);
+    }
+  });
 });
